9ec3acd60ecd43e756fccaf485d5242d2271742c
ranran <wwj@bsfit.com.cn>
2023-05-31 20:21:56 +0800

    feat(冗余代码删除):
    提交人: 王文静

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,8 @@
 /// <reference types="vite/client" />
 declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
 
-  import type { DefineComponent } from 'vue';
+  const vueComponent: DefineComponent<{}, {}, any>;
 
-  const vueComponent: DefineComponent<{}, {}, any>;
-
-  export default vueComponent;
-
-} 作者：迷斯特航 https://www.bilibili.com/read/cv19000250?from=articleDetail 出处：bilibili
+  export default vueComponent;
+}
